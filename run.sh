34714c78f8f20ba260d50a24b3d43d6c50eeb835
dragos <petrescudragos1995@gmail.com>
2018-03-05 03:59:12 +0200

    run with arguments

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
 
 
 scp *.c student@172.16.245.130:/home/student/Tema1
 scp *.h student@172.16.245.130:/home/student/Tema1
-
+scp Makefile student@172.16.245.130:/home/student/Tema1
+scp input.in student@172.16.245.130:/home/student/Tema1
+ssh student@172.16.245.130 /home/student/Tema1/run.sh 256 input.in
